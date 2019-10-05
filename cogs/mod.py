35c7b7038208ad8975d6b45c5c0a146034488455
Iron <IronCodez@gmail.com>
2019-10-04 22:52:34 -0400

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -22,12 +22,18 @@ class mod(commands.Cog):
     @commands.command()
     @has_permissions(ban_members=True, administrator=True)
     async def ban(self, ctx, member : discord.Member, *, reason="The Ban Hammer Has Spoken"):
+            if not member:
+                await ctx.send("Make sure to specify a user!")
+                return
             await member.ban(reason=reason)
             await ctx.send(f'Banned {member.mention}')
 
     @commands.command()
     @has_permissions(kick_members=True, administrator=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
+        if not member:
+            await ctx.send("Make sure to specify a user!")
+            return
         if ctx.message.author.server_permissions.kick:
             await member.kick(reason=reason)
 
@@ -36,13 +42,32 @@ class mod(commands.Cog):
     async def unban(self, ctx, *, member):
             banned_users = await ctx.guild.bans()
             member_name, member_discriminator = member.split('#')
-            
+
             for ban_entry in banned_users:
                 user = ban_entry.user
-                
+
                 if(user.name, user.discriminator) == (member_name, member_discriminator):
                     await ctx.guild.unban(user)
                     await ctx.send(f'Unbanned {user.mention}')
+
+    @commands.command()
+    @has_permissions(manage_messages=True,ban_members=True,kick_members=True)
+    async def mute(self,ctx, member: discord.Member=None):
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        if not member:
+            await ctx.send("Make sure to specify a user!")
+        await member.add_role(role)
+        await ctx.send(f'Muted {member}')
+
+    @commands.command()
+    @has_permissions(manage_messages=True,ban_members=True,kick_members=True)
+    async def unmute(self,ctx, member: discord.Member=None):
+        role = discord.utils.get(ctx.guild.roles, name='Muted')
+        if not member:
+            await ctx.send("Make sure to specify a user!")
+        await member.remove_role(role)
+        await ctx.send(f'Unmuted {member}')
+
 
 def setup(bot):
     bot.add_cog(mod(bot))
