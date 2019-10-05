462f66236587e3884d72125c009756057cbf034e
Iron <IronCodez@gmail.com>
2019-10-04 22:51:46 -0400

    Update utilities.py

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -14,36 +14,46 @@ class utilities(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send("Wew, I made it over the ~waves~. `{}ms` is my latency.".format(round(self.client.latency * 1000, 3)))
+        await ctx.send(":ping_pong: Wew, I made it over the ~waves~. `{}ms` is my heartbeat (latency) :heart:.".format(round(self.client.latency * 1000, 3)))
 
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(color=discord.Color.green())
-        embed.set_author(name="Alphait Commands - pefix: a! ")
-        embed.add_field(name="Utility Commands:", value="`a!ping` - **Sends the bot latency.** `a!help` - **Shows this message** `a!echo` - **repeats you** `a!invite` - gives an invite to invite Alphait!")
-        embed.add_field(name="Moderation Commands:", value="`a!purge` - **Clears messages. (a!clear {number})** `a!ban` - **Bans a member (a!ban {user}** `a!kick` - **Kicks a member (a!kick {user})** `a!unban` - **Unbans a member (a!unban {user}**)")
-        embed.add_field(name="Interaction Commands:", value="`a!dab` - **dab on 'em haters (a!dab {user}** `a!default` - **default on 'em haters (a!default {user}**")
-        embed.add_field(name="Info Commands:", value="`a!userinfo` - **Shows info about a user. (a!userinfo {user)**")
-        embed.add_field(name="Fun Commands:", value= "`a!8ball` - **a 8ball**, `a!coinflip` - flips a coin `a!rps` **it's rock paper scissors")
+        embed.set_author(name="Alphait V2.5.6 Commands - prefix: a!")
+        embed.add_field(name="_Utilities and infomation Commands:_", value="Useful things.")
+        embed.add_field(name="a!help", value="Sends this message.")
+        embed.add_field(name="a!ping", value="Send the latency of the bot.")
+        embed.add_field(name="a!invite", value="Sends a link to invite the bot.")
+        embed.add_field(name="a!vote", value="Sends a link to vote for the bot on a bot listing site.")
+        embed.add_field(name="a!userinfo", value="Sends infomation about the specified user.")
+        embed.add_field(name="a!serverinfo", value="Sends infomation about the server.")
+        embed.add_field(name="a!roleinfo", value="Sends infomation about the specified role.")
         await ctx.send(embed=embed)
-
-
-    @client.command()
+        embed = discord.Embed(color=discord.Color.green())
+        embed.set_author(name="Alphait V2.5.6 Commands")
+        embed.add_field(name="_Moderation Commands:_", value="Commands that you can use for moderation")
+        embed.add_field(name="a!purge (a!clear)",value="Clears the specified amount of messages")
+        embed.add_field(name="a!ban", value="Bans the specified user.")
+        embed.add_field(name="a!unban", value="Unbans the specified user.")
+        embed.add_field(name="a!kick", value="Kicks the specified user.")
+        await ctx.send(embed=embed)
+        embed = discord.Embed(color=discord.Color.green())
+        embed.set_author(name="Alphait V2.5.6 Commands")
+        embed.add_field(name="_Fun and Intercation Commands:_", value="Lists the fun and intercation commands of the bot.")
+        embed.add_field(name="a!dab", value="You dab on the specified user.")
+        embed.add_field(name="a!default", value="You default dance of the specified user.")
+        embed.add_field(name="a!8ball", value="a magic 8ball.")
+        embed.add_field(name="a!coinflip", value="Flips a coin.")
+        embed.add_field(name="a!rps", value="it's rock paper scissors.")
+        await ctx.send(embed=embed)
+        
+    @commands.command()
     async def invite(self, ctx):
         await ctx.send("Use this link to invite the bot! https://discordapp.com/oauth2/authorize?client_id=493973379515416577&permissions=8&scope=bot")
 
-    @client.command()
+    @commands.command()
     async def vote(self, ctx):
         await ctx.send("Vote Here! https://discordbots.org/bot/493973379515416577/vote")
-
-    @client.command()
-    async def avatar(self, ctx, member: discord.Member = None):
-        member = ctx.author if not member else member
-        embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
-        embed = embed.set_author(name=f"Avatar - {member.name}")
-        embed.set_image(url=member.avatar_url)
-        embed.set_footer(text=f"Requested by {ctx.author} ({ctx.author.id})", icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(utilities(bot))
